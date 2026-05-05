189743c69d93e901eb16841caf4860d678173c4f
Rodrigo Pascual Arnaiz <rpa1004@alu.ubu.es>
2026-05-05 20:47:55 +0200

    Creacion de NewReleasePrice clase extendida de Price #2

---
+++ src/ubu/gii/dass/refactoring/NewReleasePrice.java
@@ -0,0 +1,17 @@
+package ubu.gii.dass.refactoring;
+
+public class NewReleasePrice extends Price {
+
+	@Override
+	public int getPriceCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCharge(int daysRented) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
