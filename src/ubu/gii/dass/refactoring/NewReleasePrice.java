5e1005d8216977927d1c88ba76601a70c34e2663
maksimpitinov1 <mpx1019@alu.ubu.es>
2026-05-06 19:26:06 +0200

    Rellenamos getCharge() en subclase NewReleasePrice.java y sobreescribimos getFrequentRenterPoints()

--- src/ubu/gii/dass/refactoring/NewReleasePrice.java
+++ src/ubu/gii/dass/refactoring/NewReleasePrice.java
@@ -11,8 +11,11 @@ public class NewReleasePrice extends Price {
 
 	@Override
 	public double getCharge(int daysRented) {
-		// TODO Auto-generated method stub
-		return 0;
+		return daysRented * 3;
 	}
 
+	@Override
+	public int getFrequentRenterPoints(int daysRented){
+		return (daysRented > 1) ? 2 : 1;
+	}
 }
